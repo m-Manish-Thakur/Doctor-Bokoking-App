de5922cf739c7041a3e83a7977ac10634c1e2c20
Manish Thakur <manishthakur231690@gmail.com>
2024-01-17 17:28:05 +0530

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Button from "@mui/material/Button";
+const App = () => {
+  return (
+    <div>
+      <h1>Manish Thakur</h1>
+
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  );
+};
+
+export default App;
